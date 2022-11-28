20eedb185364b557f1f8e36c218b08d743235538
mcc0624 <350128075@qq.com>
2022-11-28 22:47:08 +0800

    chang class12

--- class12/1.php
+++ class12/1.php
@@ -3,14 +3,16 @@ highlight_file(__FILE__);
 error_reporting(0);
 class fast {
     public $source;
-    public function __wakeuo()    {
+    public function __wakeup(){
         echo "wakeup is here!!";
-        echo  $this->source;    }
+        echo  $this->source;
+    }
 }
 class sec {
     var $benben;
     public function __tostring(){
-        echo "tostring is here!!";    }
+        echo "tostring is here!!";
+    }
 }
 $b = $_GET['benben'];
 unserialize($b);
